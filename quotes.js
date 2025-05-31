b070a5901fc2dca49b4b5beee8f0b1138c781e9b
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2025-05-31 13:57:13 -0500

    Zelda quotes, made list neater, wrote more notes to myself

---
+++ quotes.js
@@ -0,0 +1,187 @@
+const hyrulianQuotesFake = [
+    "It's dangerous to go alone! Take this.",
+    "The Legend of Zelda is a timeless classic.",
+    "May the Triforce be with you.",
+    "Adventure awaits in Hyrule!",
+    "Link is the hero of time.",
+    "Princess Zelda is always in another castle.",
+    "Ganondorf is a formidable foe.",
+    "The Master Sword is the blade of evil's bane.",
+    "The Ocarina of Time holds great power.",
+    "Hyrule's history is rich and full of legends.",
+    "The Kokiri Forest is a magical place.",
+    "The Lost Woods can be quite tricky.",
+    "The Great Deku Tree is a wise guardian.",
+    "The Gerudo Desert is vast and mysterious.",
+    "The Zora's Domain is a serene underwater kingdom.",
+    "The Gorons are strong and resilient.",
+    "The Sheikah are masters of stealth and shadow.",
+    "The Temple of Time holds secrets of the past.",]
+
+const hyrulianQuotesReal = [
+    `A sword weilds no strength unless the hand that holds it has courage.`,
+    `It's dangerous to go alone! Take this.`,
+]
+
+const hyrulianQuoteAndAuthor = [
+    {
+        quote: `A sword weilds no strength unless the hand that holds it has courage.`,
+        author: `The Hero’s Spirit`,
+        game: `Twilight Princess`
+    },
+    {
+        quote: `It's dangerous to go alone! Take this.`,
+        author: `Old Man`,
+        game: `The Legend of Zelda`
+    },
+    {
+        quote: `The wind... it is blowing...`,
+        author: `Ganondorf`,
+        game: `The Wind Waker`
+    },
+    {
+        quote: `You've met with a terrible fate, haven't you?`,
+        author: `Happy Mask Salesman`,
+        game: `Majora's Mask`
+    },
+    {
+        quote: `No matter what, a fake is a fake, and no matter how much you try to dress it up, the real thing always wins!`,
+        author: `Midna`,
+        game: `Twilight Princess`
+    },
+    {
+        quote: `Guess what? You got it for free. Are you proud of yourself?`,
+        author: `Misc Text`,
+        game: `Link's Awakening`
+    },
+    {
+        quote: `Those who do not know the danger of wielding power will, before long, be ruled by it... Never forget that.`,
+        author: `Lanayru`,
+        game: `Twilight Princess`
+    },  
+    {
+        quote: `Your true face... What kind of... face is it? I wonder... The face under the mask... Is that... your true face?`,
+        author: `Moon Child`,
+        game: `Majora's Mask`
+    },
+    {
+        quote: `Can I ask... a question? Your friends... What kind of... people are they? I wonder... Do these people... think of you... as a friend?`, 
+        author: `Moon Child`,
+        game: `Majora's Mask`
+    },
+    {
+        quote: `Can I ask... a question? What makes you happy? I wonder... what makes you happy... does it make... others happy, too?`,
+        author: `Moon Child`,
+        game: `Majora's Mask`
+    },  
+    {
+        quote: `Whenever there is a meeting, a parting shall follow. But that parting needs not last forever. Whether a parting be forever or merely for a short while... that is up to you.`,
+        author: `Happy Mask Salesman`,
+        game: `Majora's Mask`
+    },
+    {
+        quote: `It is something that grows over time... a true friendship. A feeling in the heart that becomes even stronger over time. The passion of friendship will soon blossom into a righteous power and through it, you will know which way to go...`,
+        author: `Sheik`,
+        game: `Ocarina of Time`
+    },
+    {
+        quote: `The flow of time is always cruel... its speed seems different for each person, but no one can change it... A thing that does not change with time is a memory of younger days...`,
+        author: `Sheik`,
+        game: `Ocarina of Time`
+    },
+    {
+        quote: `Time passes, people move... Like a river's flow, it never ends... A childish mind will turn to noble ambition... Young love will become deep affection... The clear water's surface reflects growth.`,
+        author: `Sheik`,
+        game: `Ocarina of Time`
+    },
+    {
+        quote: `Well, my mind is getting hazy... Please let me hear the sound of the flute one last time...`,
+        author: `Flute Kid`,
+        game: `A Link to the Past`
+    },
+    {
+        quote: `Hey! Listen!`,
+        author: `Navi`,
+        game: `Ocarina of Time`
+    },
+    {
+        quote: `Light And Shadow Can't Mix, As We All Know. But... Never Forget That There's Another World Bound To This One.`,
+        author: `Midna`,
+        game: `Twilight Princess`
+    },
+    {
+        quote: `I Lack Sufficient Data To Be Sure Of My Conclusion, But I Believe This Feeling Correlates Closest To What Your People Call… Happiness.`,
+        author: `Fi`,
+        game: `Skyward Sword`
+    },
+    {
+        quote: `Zelda... We Rely On Your Knight, And That Legendary Sword He Carries. Our Last Line Of Defense Will Be Link.`,
+        author: `Rauru`,
+        game: `Tears Of The Kingdom`
+    },
+    {
+        quote: `Do Not Look Away. You Witness A King's Revival, And The Birth Of His New World.`,
+        author: `Ganondorf`,
+        game: `Tears Of The Kingdom`
+    },
+    {
+        quote: `Yet, After All That We’ve Seen, Do We Not Share The Same Wish? Let’s Make The Wish Together, Link.`,
+        author: `Princess Zelda`,
+        game: `A Link Between Worlds`
+    },
+    {
+        quote: `We Shall Greet The Morning… Together.`,
+        author: `Kafei And Anju`,
+        game: `Majora’s Mask`
+    },
+    {
+        quote: `The Only Distance That Matters Is The Distance Between Our Hearts.`,
+        author: `Rhondson`,
+        game: `Tears Of The Kingdom`
+    },
+    {
+        quote: `If I Was A Seagull, I Would Fly As Far As I Could! I Would Fly To Faraway Places And Sing For Many People! …If I Wish To The Wind Fish, I Wonder If My Dream Will Come True...`,
+        author: `Marin`,
+        game: `Link’s Awakening`
+    },
+    {
+        quote: `I Want You To Live For The Future. There May Be Nothing Left For You… But Despite That, You Must Look Forward And Walk A Path Of Hope, Trusting That It Will Sustain You When Darkness Comes.`,
+        author: `King Of Hyrule`,
+        game: `The Wind Waker`
+    },
+    {
+        quote: `An Incarnation Of My Hatred Shall Ever Follow Your Kind, Dooming Them To Wander A Blood-Soaked Sea Of Darkness For All Time!`,
+        author: `Demise`,
+        game: `Skyward Sword`
+    },
+    {
+        quote: `I Wasn’t Kidding When I Said Pay! Now You’ll Pay The Ultimate Price!`,
+        author: `Shopkeeper`,
+        game: `Link’s Awakening`
+    },
+    {
+        quote: `Courage Need Not Be Remembered, For It Is Never Forgotten.`,
+        author: `Princess Zelda`,
+        game: `Breath Of The Wild`
+    },
+    {
+        quote: `Do You Ever Feel A Strange Sadness As Dusk Falls? They Say It's The Only Time When Our World Intersects With Theirs... [...] That Is Why Loneliness Always Pervades The Hour Of Twilight.`,
+        author: `Rusl`,
+        game: `Twilight Princess`
+    },
+    {
+        quote: `The Right Thing… What Is It? I Wonder… If You Do The Right Thing… Does It Really Make… Everybody… Happy?`,
+        author: `Moon Child`,
+        game: `Majora’s Mask`
+    }
+]
+
+export { hyrulianQuotesFake, hyrulianQuotesReal, hyrulianQuoteAndAuthor }
+
+/* ALL QUOTES FROM A REDDIT POST
+
+Misc
+
+https://www.reddit.com/r/zelda/comments/11f1b2/what_are_your_favorite_quotes_from_the_zelda_games/
+
+*/
--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 import { getSomeItems, getAllItems, getItemByID, getItemByName } from './get-functions.js'
+import { hyrulianQuoteAndAuthor } from './quotes.js'
+
+const titleRupeeImage = document.querySelectorAll('.title-rupee-image')
 
 const resultsList = document.getElementById('results-list')
 const resultsTitle = document.getElementById('results-title')
 const searchInput = document.getElementById('search-input')
 const searchButton = document.getElementById('search-button')
 const categoryButtons = document.querySelectorAll('.category-button')
+
+const quoteContainer = document.getElementById('quote-container')
+const zeldaQuote = document.getElementById('zelda-quote')
+const zeldaQuoteAuthor = document.getElementById('zelda-quote-author')
+const zeldaQuoteGame = document.getElementById('zelda-quote-game')
+const quoteButton = document.getElementById('quote-button')
+
+const rupeeImageList = ['./images/green-rupee-1.png', './images/green-rupee-2.png', './images/orange-rupee.png']
+
+
+// Display a random Hyrulian quote and a random Rupee color on page load
+let randomRupee = randNum(rupeeImageList.length)
+let randomQuote = randNum(hyrulianQuoteAndAuthor.length)
+
+
+loadQuote(randomQuote)
+quoteButton.addEventListener('click', function () {
+    randomQuote = randNum(hyrulianQuoteAndAuthor.length)
+    loadQuote(randomQuote)
+})
+
+for (let rupeeImage of titleRupeeImage) {
+    rupeeImage.src = rupeeImageList[randomRupee]
+}
 
 for (let button of categoryButtons) {
 
@@ -39,4 +66,20 @@ for (let button of categoryButtons) {
 
 function clearResults() {
     resultsList.innerHTML = ''
+}
+
+/* Returns a random number between 0 and the length of given array */
+/* Used for randomizing flavot text for website */
+function randNum(maxNum) {
+
+    let randIndex = Math.floor(Math.random() * maxNum) 
+    return randIndex
+}
+
+function loadQuote(randomNumber) {
+    zeldaQuote.textContent = hyrulianQuoteAndAuthor[randomNumber].quote
+    zeldaQuoteAuthor.textContent = hyrulianQuoteAndAuthor[randomNumber].author
+    zeldaQuoteGame.textContent = hyrulianQuoteAndAuthor[randomNumber].game
+    if (quoteButton.textContent === ``)
+        quoteButton.textContent = 'Reload quote'
 }
